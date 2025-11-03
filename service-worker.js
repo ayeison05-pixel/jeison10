a34931ac4d553c4c55fcfc4b749de38937337cf9
ayeison05-pixel <a.yeison05@gmail.com>
2025-11-03 08:27:34 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'mototaxi-jeison-v1';
+const urlsToCache = [
+  '/jeison10/',
+  '/jeison10/index.html',
+  '/jeison10/manifest.json',
+  '/jeison10/icon-192.png',
+  '/jeison10/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((err) => console.warn('Error al cachear:', err))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/jeison10/')) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => response || fetch(event.request))
+        .catch(() => {
+          if (event.request.destination === 'document') {
+            return caches.match('/jeison10/index.html');
+          }
+        })
+    );
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheAllowlist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheAllowlist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
